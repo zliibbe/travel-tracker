6016c95d85e075aabba76d042ac239eecfa2bfba
Zach Liibbe <zliibbe@gmail.com>
2022-03-02 22:24:26 -0700

    Add test to destination-test.js

--- test/destination-test.js
+++ test/destination-test.js
@@ -1,2 +1,36 @@
 import chai from 'chai';
 const expect = chai.expect;
+
+describe('Traveler', function() {
+    let destination1;
+    let destination2;
+
+    beforeEach(() => {
+        destination1 = {
+            "id": 1,
+            "destination": "Lima, Peru",
+            "estimatedLodgingCostPerDay": 70,
+            "estimatedFlightCostPerPerson": 400,
+            "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+            "alt": "overview of city buildings with a clear sky"
+            };
+        destination2 = {
+            "id": 2,
+            "destination": "Stockholm, Sweden",
+            "estimatedLodgingCostPerDay": 100,
+            "estimatedFlightCostPerPerson": 780,
+            "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+            "alt": "city with boats on the water during the day time"
+            };
+      });
+
+    it('should be a function', () => {
+        expect(Destination).to.be.a('function')
+    })
+
+    it('should instantiate Destination', () => {
+        expect(destination1).to.be.an.instanceOf(Destination);
+    });
+
+
+})
