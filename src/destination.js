cd7ce5ed74026860461707f296429405264b2fcc
Zach Liibbe <zliibbe@gmail.com>
2022-03-03 12:52:25 -0700

    Edit constructor

---
+++ src/destination.js
@@ -0,0 +1,10 @@
+class Destination {
+    constructor() {
+        this.id = 
+        this.destination = 
+        this.estimatedLodgingCostPerDay = 
+        this.estimatedFlightCostPerPerson = 
+        this.image = 
+        this.alt = 
+    }
+}
