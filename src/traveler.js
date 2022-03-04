42681ec73f2bfd9516c012203cd6268753ca6f4c
Zach Liibbe <zliibbe@gmail.com>
2022-03-04 15:59:46 -0700

    Add values to properties and getFirstName method

--- src/traveler.js
+++ src/traveler.js
@@ -1,8 +1,23 @@
 class Traveler {
-    constructor() {
-        this.id = 
-        this.name = 
-        this.type = 
+    constructor(traveler) {
+        this.id = traveler.id
+        this.name = traveler.name
+        this.type = traveler.travelerType
         this.trips = []
     }
+
+    getFirstName() {
+        let fullName = this.name.split(' ')
+        return fullName[0]
+    }
+    
+    // getTrips(travelerID) {
+    //     
+    // }
+
+    getDestinations(travelerID) {
+
+    }
 }
+
+export default Traveler;
