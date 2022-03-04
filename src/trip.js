5cb958dd1ae62faf7a42c6726c5f786443c34f30
Zach Liibbe <zliibbe@gmail.com>
2022-03-04 16:05:15 -0700

    Add values to class properties

--- src/trip.js
+++ src/trip.js
@@ -1,13 +1,14 @@
 class Trip {
-    constructor() {
-        this.id = #
-        this.userID = #
-        this.destinationID = #
-        this.travelers = #
-        this.date = ''
-        this.duration = #
-        this.status = ''
-        this.suggestedActivities = []
-
+    constructor(trip) {
+        this.id = trip.id
+        this.userID = trip.userID
+        this.destinationID = trip.destinationID
+        this.travelers = trip.travelers
+        this.date = trip.date
+        this.duration = trip.duration
+        this.status = trip.status
+        this.suggestedActivities = trip.suggestedActivities
     }
 }
+
+export default Trip
