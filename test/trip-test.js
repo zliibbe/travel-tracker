58c915654e12eaf4a750b3f28d5c7dd9a6aff69c
Zach Liibbe <zliibbe@gmail.com>
2022-03-06 21:50:33 -0700

    Add variables prior to beforeEach and unskip tests

--- test/trip-test.js
+++ test/trip-test.js
@@ -1,13 +1,17 @@
 import chai from 'chai';
 import Traveler from '../src/traveler'
 import Trip from '../src/trip';
+import Destination from '../src/destination';
 const expect = chai.expect;
 
 describe('Trip', function() {
-    let traveler1
-    let traveler2
     let travelerOneData
     let travelerTwoData
+    let traveler1
+    let traveler2
+    let tripData
+    let trip1
+    let destination1
 
     beforeEach(() => {
         travelerOneData = {
@@ -33,40 +37,51 @@ describe('Trip', function() {
                 "duration": 11,
                 "status": "pending",
                 "suggestedActivities": []
-                }
-        trip1 = new Trip(tripData)
-        
-      });
+                };
 
-    it.skip('should be a function', () => {
+        destination1 = new Destination()
+
+        trip1 = new Trip(tripData, destination1)  
+    });
+
+    it('should be a function', () => {
         expect(Traveler).to.be.a('function')
     })
 
-    it.skip('should have a trip id', () => {
-        expect(trave.id).to.eql(71)
+    it('should have a trip id', () => {
+        expect(tripData.id).to.eql(71)
     })
 
-    it.skip('should have a userID of the person going on the trip', () => {
-        expect(trip.userID).to.eql(38)
+    it('should have a userID of the person going on the trip', () => {
+        expect(trip1.userID).to.eql(38)
     })
 
-    it.skip('should have an destinationID for the location that the trip is going to', () => {
-        expect(trip.destinationID).to.eql(28)
+    it('should have an destinationID for the location that the trip is going to', () => {
+        expect(trip1.destinationID).to.eql(28)
     })
 
-    it.skip('should have a number of travelers that are going on the trip', () => {
-        expect(trip.travelers).to.eql(1)
+    it('should have a number of travelers that are going on the trip', () => {
+        expect(trip1.travelers).to.eql(1)
     })
 
-    it.skip('should have a date that the trip starts on', () => {
-        expect(trip.date).to.eql("2020/05/26")
+    it('should have a date that the trip starts on', () => {
+        expect(trip1.date).to.eql("2020/05/26")
     })
 
-    it.skip('should have a duration', () => {
-        expect(trip.duration).to.eql(11)
+    it('should have a duration', () => {
+        expect(trip1.duration).to.eql(11)
     })
 
-    it.skip('should have a status', () => {
-        expect(trip.status).to.eql("pending")
+    it('should have a status', () => {
+        expect(trip1.status).to.eql("pending")
     })
+
+    it.skip('should be able to find a destination', () => {
+
+    })
+
+    it.skip('should be able to calculate the cost of a trip with 10% fee added')
+
+   
+
 })
