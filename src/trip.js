177cea4aa7f0b7fc43eb7aa4b1edd5533d0b510a
Zach Liibbe <zliibbe@gmail.com>
2022-03-06 21:46:09 -0700

    Update trip class constructor and add findDestination and costOfTrip methods

--- src/trip.js
+++ src/trip.js
@@ -1,15 +1,30 @@
+
 class Trip {
-    constructor(trip) {
-        this.id = trip.id
-        this.userID = trip.userID
-        this.destinationID = trip.destinationID
-        this.travelers = trip.travelers
-        this.date = trip.date
-        this.duration = trip.duration
-        this.status = trip.status
-        this.suggestedActivities = trip.suggestedActivities
+    constructor(tripObj, destinationsData) {
+        this.id = tripObj.id
+        this.userID = tripObj.userID
+        this.destinationID = tripObj.destinationID
+        this.travelers = tripObj.travelers
+        this.date = tripObj.date
+        this.duration = tripObj.duration
+        this.status = tripObj.status
+        this.suggestedActivities = tripObj.suggestedActivities
+        this.cost = this.costOfTrip(destinationsData)
     }
 
+    findDestination = (destinationsData) => {
+        // console.log(destinationsData, "destinationsData###")
+        // console.log(destinationsData, "DestData(in trip.js)")
+        return destinationsData.find(destination => destination.id === this.destinationID)
+    }
+
+    costOfTrip(destinationData) {
+        const destinationObj = this.findDestination(destinationData)
+        const flightCost = destinationObj.estimatedFlightCostPerPerson * this.travelers
+        const lodgingCost = destinationObj.estimatedLodgingCostPerDay * this.duration * this.travelers
+        // console.log(destinationObj, flightCost, lodgingCost, "destination | flightCost | lodingCost")
+        return Math.round(1.1 * (flightCost + lodgingCost))
+    }
 }
 
-export default Trip
+export default Trip;
