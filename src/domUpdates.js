dbf191051b9087bcedb8d187f39bdffd1442b569
Zach Liibbe <zliibbe@gmail.com>
2022-03-07 14:20:21 -0700

    Add insertAdjacentHTML for adding destinations

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -12,23 +12,29 @@ const domUpdates = {
 
     displayAnnualSpending(userAnnualSpending) {
         const annualSpending = document.querySelector('.annual-expenditure')
-        annualSpending.innerText = `You've spent ${userAnnualSpending} in ${dayjs().format('YYYY')}`
+        annualSpending.innerText = `You've spent ${userAnnualSpending} in ${dayjs().format('YYYY')}.`
     },
 
     displayTrips(traveler, tripsData, destinationsData) {
         traveler.getUsersTrips(tripsData, destinationsData).forEach(trip =>{
         const place = trip.findDestination(destinationsData)
-        // console.log(place, "place-- in domUpdates")
-        tripList.insertAdjacentHTML('beforeend',`<article class="trip">
-        <img class="trip-img" src=${place.image} alt=${place.alt}> 
-        <h2 class="trip-destination">${place.destination}</h2>
-        <h3 clss="trip-status">Status: ${trip.status}</h3>
+        tripList.insertAdjacentHTML('beforeend',
+        `<article class="trip">
+        <div class="trip-container">
+        <div class="trip-heading-container">
+            <div class="trip-headings">    
+                <h2 class="trip-destination">${place.destination}</h2>
+                <h3 class="trip-status">Status: ${trip.status}</h3>
+        </div>    
         <div class="trip-information">
-          <p>start date: ${dayjs(trip.date).format("MMMM DD, YYYY")}</p>
-          <p>duration: ${trip.duration}</p>
-          <p>travelers: ${trip.travelers}</p>
-          <p>cost: $${trip.cost}</p>
+            <p>start date: ${dayjs(trip.date).format("MMMM DD, YYYY")}</p>
+            <p>duration: ${trip.duration}</p>
+            <p>travelers: ${trip.travelers}</p>
+            <p>cost: $${trip.cost}</p>
         </div>
+        </div>
+        <img class="trip-img" src=${place.image} alt=${place.alt}>
+        </div>    
       </article>`);})
     },
 };
