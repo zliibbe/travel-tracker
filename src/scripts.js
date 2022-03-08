43a1f66587b1be5e2dbcfd4c010fc2800a9432b6
Zach Liibbe <zliibbe@gmail.com>
2022-03-08 09:38:25 -0700

    Add POST request and update DOM for POST

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/profilepic'
 // import './images/favicon'
-import {fetchDataFor} from './apiCalls'
+import {fetchDataFor, postData} from './apiCalls'
 import Traveler from './traveler'
 import Trip from './trip'
 import Destination from './destination';
@@ -16,6 +16,14 @@ let destinationsData
 let tripsData
 let userDestinations
 
+//querySelectors
+const tripDate = document.querySelector('.date-input');
+const tripNumOfDays = document.querySelector('.number-of-days-input');
+const tripNumOfTravelers = document.querySelector('.number-of-travelers-input');
+const tripDestination = document.querySelector('.destination-input');
+const newTripForm = document.querySelector('.new-trip-form');
+const newTripConfirmation = document.querySelector('.new-trip-confirmation');
+
 // function declaration
 const getAllData = () => {
     return Promise.all([fetchDataFor(`travelers`), 
@@ -25,7 +33,7 @@ const getAllData = () => {
         travelersData = data[0].travelers;
         destinationsData = data[1].destinations;
         tripsData = data[2].trips;
-        user = new Traveler(getByID(travelersData, '14'))
+        user = new Traveler(getByID(travelersData, '38'))
         user.trips = user.getUsersTrips(tripsData, destinationsData)//instantiate Trip class in traveler.js
         userDestinations = user.getUsersDestinations(destinationsData)//instantiate Dest class in traveler.js  
     }).then(() =>{
@@ -41,6 +49,36 @@ const getByID = (travelers, id) => {
     })
 }
 
+const postNewTrip = (e) => {
+    e.preventDefault();
+    const newTripRequest = {
+        id: tripsData.length+1,
+        userID: user.id, 
+        destinationID: parseInt(tripDestination.value),
+        travelers: parseInt(tripNumOfTravelers.value), 
+        date: tripDate.value.replaceAll('-', '/'),
+        duration: parseInt(tripNumOfDays.value), 
+        status: 'pending', 
+        suggestedActivities: [],
+    }
+    postData('trips', newTripRequest);//POST trip to server
+    user.trips.push(newTripRequest);//push trip to user's trips array
+    domUpdates.newTripRequested(newTripConfirmation)//display post success & then hide this msg
+    Promise.all([fetchDataFor(`travelers`), 
+                        fetchDataFor('destinations'), 
+                        fetchDataFor('trips')])
+    .then(data => {
+        travelersData = data[0].travelers;
+        destinationsData = data[1].destinations;
+        tripsData = data[2].trips;
+        user.trips = user.getUsersTrips(tripsData, destinationsData)
+        userDestinations = user.getUsersDestinations(destinationsData)
+    }).then(() =>{
+        updateDOM(user, user.trips, userDestinations)//redisplay new info to DOM
+    })
+    e.target.reset();
+}
+
 const updateDOM = (userData, tripsData, destinationsData) => {
     domUpdates.userGreeting(userData.getFirstName())
     domUpdates.displayAnnualSpending(userData.annualSpent())
@@ -49,3 +87,4 @@ const updateDOM = (userData, tripsData, destinationsData) => {
 
 //eventListeners 
 window.addEventListener('load', getAllData());
+newTripForm.addEventListener('submit', postNewTrip);
